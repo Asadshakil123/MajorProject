7a2291936724baf327a495c8233a9e73e72da750
Mohammad Asad Shakil <asadshakil340@gmail.com>
2024-10-01 14:10:58 +0530

    commit message

--- init/index.js
+++ init/index.js
@@ -19,6 +19,8 @@ async function main() {
 
 const initDB = async () => {
    await Listing.deleteMany({});
+   initData.data = initData.data.map((obj) => ({...obj, 
+    owner:'66dfe86ebc565a3c900c53fe'}));
    await Listing.insertMany(initData.data);  
    console.log("data was initialized");
 };
--- models/listing.js
+++ models/listing.js
@@ -1,23 +1,49 @@
 const mongoose = require("mongoose");
+const review = require("./review");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
+const { ref, required } = require("joi");
 
 const listingSchema = new Schema({
-   /* title: {
+    title: {
         type: String,
         required: true,
-    },*/
+    },
     description: String,
     image: {
-        type: String,
-        default:  "https://images.unsplash.com%252fphoto-1541526855570-9154d61f6a8b%253fixlib%253drb-1.2.1%2526q%253d80%2526fm%253djpg%2526crop%253dentropy%2526cs%253dtinysrgb%2526w%253d1080%2526fit%253dmax%26ehk%3dfl07u6e2UzwDXpgL57bYq38KFYHBJCIDmjbhJ%252bpDMoE%253d%26risl%3d%26pid%3dImgRaw%26r%3d0&exph=1620&expw=1080&q=unsplash&simid=608046539522726255&FORM=IRPRST&ck=BF7177286C76C81FDE59C8A1C014D4B9&selectedIndex=3&itb=0&ajaxhist=0&ajaxserp=0" ,
-        set: (v) => 
-        v === "" ? 
-        "https://images.unsplash.com%252fphoto-1541526855570-9154d61f6a8b%253fixlib%253drb-1.2.1%2526q%253d80%2526fm%253djpg%2526crop%253dentropy%2526cs%253dtinysrgb%2526w%253d1080%2526fit%253dmax%26ehk%3dfl07u6e2UzwDXpgL57bYq38KFYHBJCIDmjbhJ%252bpDMoE%253d%26risl%3d%26pid%3dImgRaw%26r%3d0&exph=1620&expw=1080&q=unsplash&simid=608046539522726255&FORM=IRPRST&ck=BF7177286C76C81FDE59C8A1C014D4B9&selectedIndex=3&itb=0&ajaxhist=0&ajaxserp=0" 
-        : v,
+       url: String,
+       filename: String,
     },
     price: Number,
     location: String,
     country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+       },
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    geometry: {
+        type: { 
+            type: String,
+            enum: ['Point'],
+            required: true,
+        },
+        coordinates: {
+            type: [ Number ],
+            required: true,
+        },
+    },
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if(listing) {
+    await Review.deleteMany({_id: {$in: listing.reviews} });
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
